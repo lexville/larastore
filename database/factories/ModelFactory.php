9fdb69f3293f8d25a53a74d6e4f776241e116304
lexville <alex.kangethe@andela.com>
2016-07-26 10:24:08 +0300

    (Chore) Add store factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,11 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Store::class, function (Faker\Generator $faker) {
+    return [
+        'name' => $faker->name,
+        'description' => $faker->description,
+        'user_id' => rand(1, 5),
+    ];
+});
