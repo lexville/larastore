fd821a6c2e70088ce8e792582ab1a1b3e440fda0
lexville <alex.kangethe@andela.com>
2016-07-26 12:28:54 +0300

    (Fix) Edit product controllers

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\Store;
 use App\Product;
 
 class ProductsController extends Controller
@@ -14,13 +15,17 @@ class ProductsController extends Controller
         return view('products.index');
     }
 
-    public function postProduct(Request $request)
+    public function postProduct(Request $request, $id)
     {
+        $store = Store::findOrFail($id);
+
         $newProduct = new Product;
         $newProduct->product_name = $request->input('product_name');
         $newProduct->product_description = $request->input('product_description');
         $newProduct->product_price = $request->input('product_price');
+        $newProduct->store_id = $store->id;
+        $newProduct->user_id = auth()->user()->id;
 
-        $request->user()->store()->products()->save($newProduct);
+        $newProduct->save();
     }
 }
