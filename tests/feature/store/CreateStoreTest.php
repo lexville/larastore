7c1efe3493faf7745dfd89b47c3b66f8c2ca5a61
lexville <alex.kangethe@andela.com>
2016-07-26 10:13:47 +0300

    (Feature) Create users ability to create a store test

---
+++ tests/feature/store/CreateStoreTest.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use App\Store;
+use App\User;
+
+class CreateStoreTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testUserCanCreateStore()
+    {
+        $user = factory(User::class)->create();
+
+        $this->actingAs($user)
+            ->visit('/store/create')
+            ->type('New Store', 'store')
+            ->type('Store Description', 'description')
+            ->click('Add New Store')
+            ->seeInDatabase('stores', ['name' => 'New Store']);
+    }
+
+    public function testNumberOfStoresIncreasesInDatabase()
+    {
+        factory(Store::class, 10)->create();
+
+        $allStores = Store::count();
+
+        $this->count(10, $allStores);
+    }
+}
